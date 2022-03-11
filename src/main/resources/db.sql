f595ba3e9404e8759401738afc37c6a72520854c
Patrick Ji <jpch89@outlook.com>
2022-03-11 16:28:31 +0800

    prepare database

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -11,3 +11,74 @@ CREATE TABLE `t_user`
     PRIMARY KEY (`id`)
 ) ENGINE = INNODB
   DEFAULT CHARSET = utf8mb4 COMMENT = '用户';
+
+CREATE TABLE `t_goods`
+(
+    `id`           BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '商品id',
+    `goods_name`   VARCHAR(16)    DEFAULT NULL COMMENT '商品名称',
+    `goods_title`  VARCHAR(64)    DEFAULT NULL COMMENT '商品标题',
+    `goods_img`    VARCHAR(64)    DEFAULT NULL COMMENT '商品图片',
+    `goods_detail` LONGTEXT COMMENT '商品描述',
+    `goods_price`  DECIMAL(10, 2) DEFAULT '0.00' COMMENT '商品价格',
+    `goods_stock`  INT(11)        DEFAULT '0' COMMENT '商品库存,-1表示没有限制',
+    PRIMARY KEY (`id`)
+) ENGINE = INNODB
+  AUTO_INCREMENT = 3
+  DEFAULT CHARSET = utf8mb4 COMMENT = '商品';
+
+CREATE TABLE `t_order`
+(
+    `id`               BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '订单ID',
+    `user_id`          BIGINT(20)     DEFAULT NULL COMMENT '用户ID',
+    `goods_id`         BIGINT(20)     DEFAULT NULL COMMENT '商品ID',
+    `delivery_addr_id` BIGINT(20)     DEFAULT NULL COMMENT '收获地址ID',
+    `goods_name`       VARCHAR(16)    DEFAULT NULL COMMENT '冗余过来的商品名称',
+    `goods_count`      INT(20)        DEFAULT '0' COMMENT '商品数量',
+    `goods_price`      DECIMAL(10, 2) DEFAULT '0.00' COMMENT '商品单价',
+    `order_channel`    TINYINT(4)     DEFAULT '0' COMMENT '1 pc, 2 android, 3 ios',
+    `status`           TINYINT(4)     DEFAULT '0' COMMENT '订单状态，0新建未支付，1已支付，2已发货，3已收货，4已退货，5已完成',
+    `create_date`      datetime       DEFAULT NULL COMMENT '订单创建时间',
+    `pay_date`         datetime       DEFAULT NULL COMMENT '支付时间',
+    PRIMARY KEY (`id`)
+) ENGINE = INNODB
+  AUTO_INCREMENT = 12
+  DEFAULT CHARSET = utf8mb4 COMMENT = '订单';
+
+CREATE TABLE `t_miaosha_goods`
+(
+    `id`            BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+    `goods_id`      BIGINT(20)     DEFAULT NULL COMMENT '商品ID',
+    `miaosha_price` DECIMAL(10, 2) DEFAULT '0.00' COMMENT '秒杀价',
+    `stock_count`   INT(10)        DEFAULT NULL COMMENT '库存数量',
+    `start_date`    datetime       DEFAULT NULL COMMENT '秒杀开始时间',
+    `end_date`      datetime       DEFAULT NULL COMMENT '秒杀结束时间',
+    PRIMARY KEY (`id`)
+) ENGINE = INNODB
+  AUTO_INCREMENT = 3
+  DEFAULT CHARSET = utf8mb4 COMMENT = '秒杀商品';
+
+CREATE TABLE `t_miaosha_order`
+(
+    `id`       BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '秒杀订单ID',
+    `user_id`  BIGINT(20) DEFAULT NULL COMMENT '用户ID',
+    `order_id` BIGINT(20) DEFAULT NULL COMMENT '订单ID',
+    `goods_id` BIGINT(20) DEFAULT NULL COMMENT '商品ID',
+    PRIMARY KEY (`id`)
+) ENGINE = INNODB
+  AUTO_INCREMENT = 3
+  DEFAULT CHARSET = utf8mb4 COMMENT = '秒杀订单';
+
+INSERT INTO `t_goods`
+VALUES (1, 'IPHONE 12', 'IPHONE 12 64GB', 'IPHONE 12 64GB', '/img/iphone12.png', '6299.00', 100),
+       (2,
+        'IPHONE 12 PRO',
+        'IHPONE 12 PRO 128GB',
+        'IPHONE 12 PRO 128GB',
+        '/img/iphone12pro.png',
+        '9299.00',
+        100);
+
+-- 注意：秒杀商品的库存一定要低于商品的库存
+INSERT INTO `t_miaosha_goods`
+VALUES (1, 1, '629', 10, '2022-11-11 08:00:00', '2022-11-11 09:00:00'),
+       (2, 2, '929', 10, '2022-11-11 08:00:00', '2022-11-11 09:00:00');
