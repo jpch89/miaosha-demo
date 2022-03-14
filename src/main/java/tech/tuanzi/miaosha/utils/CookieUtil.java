3a39bf351ce24939733c6ae881a750d903d78b16
Patrick Ji <jpch89@outlook.com>
2022-03-14 19:33:11 +0800

    fix bug in CookieUtil.java

--- src/main/java/tech/tuanzi/miaosha/utils/CookieUtil.java
+++ src/main/java/tech/tuanzi/miaosha/utils/CookieUtil.java
@@ -209,7 +209,9 @@ public final class CookieUtil {
             int len = domains.length;
             if (len > 3) {
                 // www.xxx.com.cn
-                domainName = domains[len - 3] + "." + domains[len - 2] + "." + domains[len - 1];
+                // domainName = domains[len - 3] + "." + domains[len - 2] + "." + domains[len - 1];
+                // 为了适配 127.0.0.1 这种情形而修改
+                domainName = domains[len - 4] + "." + domains[len - 3] + "." + domains[len - 2] + "." + domains[len - 1];
             } else if (len <= 3 && len > 1) {
                 // xxx.com or xxx.cn
                 domainName = domains[len - 2] + "." + domains[len - 1];
--- src/test/java/tech/tuanzi/miaosha/MiaoshaApplicationTests.java
+++ src/test/java/tech/tuanzi/miaosha/MiaoshaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.miaosha;
+package tech.tuanzi.miaosha;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
