434a0539347c4c79e4b87332b7b97a1cb6aadaac
Patrick Ji <jpch89@outlook.com>
2022-03-15 15:08:51 +0800

    miaosha static

--- src/main/java/tech/tuanzi/miaosha/controller/MiaoshaController.java
+++ src/main/java/tech/tuanzi/miaosha/controller/MiaoshaController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import tech.tuanzi.miaosha.entity.MiaoshaOrder;
 import tech.tuanzi.miaosha.entity.Order;
 import tech.tuanzi.miaosha.entity.User;
@@ -12,6 +14,7 @@ import tech.tuanzi.miaosha.service.IGoodsService;
 import tech.tuanzi.miaosha.service.IMiaoshaOrderService;
 import tech.tuanzi.miaosha.service.IOrderService;
 import tech.tuanzi.miaosha.vo.GoodsVo;
+import tech.tuanzi.miaosha.vo.RespBean;
 import tech.tuanzi.miaosha.vo.RespBeanEnum;
 
 /**
@@ -34,8 +37,40 @@ public class MiaoshaController {
      * Windows 优化前 QPS：785
      * Linux 优化前 QPS：170
      */
-    @RequestMapping("/doMiaosha")
-    public String doMiaosha(Model model, User user, Long goodsId) {
+    @RequestMapping(value = "/doMiaosha", method = RequestMethod.POST)
+    @ResponseBody
+    public RespBean doMiaosha(Model model, User user, Long goodsId) {
+        if (user == null) {
+            return RespBean.error(RespBeanEnum.SESSION_ERROR);
+        }
+
+        GoodsVo goods = goodsService.findGoodsVoByGoodsId(goodsId);
+        // 判断库存
+        if (goods.getStockCount() < 1) {
+            model.addAttribute("errmsg", RespBeanEnum.EMPTY_STOCK.getMessage());
+            return RespBean.error(RespBeanEnum.EMPTY_STOCK);
+        }
+
+        // 判断是否重复抢购
+        MiaoshaOrder miaoshaOrder = miaoshaOrderService.getOne(
+                new QueryWrapper<MiaoshaOrder>().eq("user_id", user.getId()).eq("goods_id", goodsId)
+        );
+        if (miaoshaOrder != null) {
+            model.addAttribute("errmsg", RespBeanEnum.REPEAT_ERROR.getMessage());
+            return RespBean.error(RespBeanEnum.REPEAT_ERROR);
+        }
+
+        Order order = orderService.miaosha(user, goods);
+        return RespBean.success(order);
+    }
+
+    /**
+     * 秒杀
+     * Windows 优化前 QPS：785
+     * Linux 优化前 QPS：170
+     */
+    @RequestMapping(value = "/doMiaosha2")
+    public String doMiaosha2(Model model, User user, Long goodsId) {
         if (user == null) {
             return "login";
         }
